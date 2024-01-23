609a7769208211fc5cf72d4a0fa1084914c2d09e
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-01-23 15:41:35 -0300

    Update bri_app_form.py

--- bri_app_form.py
+++ bri_app_form.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
+#import plotly.express as px
 
 # Display Title and Description
-st.title("Vendor Management Portal")
+st.title("Teste BRI")
+
+
